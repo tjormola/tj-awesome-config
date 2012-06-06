71533636212270644fc84c0716ed24ed00ed7e6f
Tuomas Jormola <tj@solitudo.net>
2012-06-06 21:52:18 +0300

    Format some white spaces

--- rc.lua
+++ rc.lua
@@ -513,11 +513,11 @@ awful.rules.rules = {
 	{
 		rule       = { },
 		properties = {
-  				       border_width = beautiful.border_width,
-				       border_color = beautiful.border_normal,
-				       focus        = true,
-				       keys         = clientkeys,
-				       buttons      = clientbuttons
+  					   border_width = beautiful.border_width,
+					   border_color = beautiful.border_normal,
+					   focus        = true,
+					   keys         = clientkeys,
+					   buttons      = clientbuttons
 		}
 	},
 	{
